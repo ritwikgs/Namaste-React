ad21d4f30417806e14f6c51efccde87e713e367e
ritwikgs <ritwik.gs@gmail.com>
2024-01-09 15:53:14 +0530

    basic-react

---
+++ App.js
@@ -0,0 +1,5 @@
+const root = ReactDOM.createRoot(document.getElementById("root"));
+const elementWithHeading = React.createElement("div", {id: "parent"}, React.createElement("div", {id: "child"}, React.createElement("h1", {}, "Nested Heading")));
+root.render(elementWithHeading);
+console.log(root);
+console.log(elementWithHeading);
