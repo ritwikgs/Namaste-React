7050fe692bc4a6e7a53d4b9d28381ed9a9995200
ritwikgs <ritwik.gs@gmail.com>
2024-01-10 13:28:56 +0530

    parcel-added-npm

--- App.js
+++ App.js
@@ -1,5 +1,18 @@
+import React from "react";
+import ReactDOM from "react-dom";
+
+
+const parent = React.createElement("div", { id: "parent" }, [
+  React.createElement("div", { id: "child" }, [
+      React.createElement("h1", {}, "I'm an h1 tag "),
+      React.createElement("h2", {}, "I'm an h2 tag "),
+    ]),
+    React.createElement("div", { id: "child2" }, [
+        React.createElement("h1", {}, "I'm an h1 tag "),
+        React.createElement("h2", {}, "I'm an h2 tag "),
+  ]),
+]);
 const root = ReactDOM.createRoot(document.getElementById("root"));
-const elementWithHeading = React.createElement("div", {id: "parent"}, React.createElement("div", {id: "child"}, React.createElement("h1", {}, "Nested Heading")));
-root.render(elementWithHeading);
+root.render(parent);
 //console.log(root);
 //console.log(elementWithHeading);
--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # Namaste-React
+
 Learn React
+HMR - Hot module replacement by file watching algorithm(C++) by parcel
