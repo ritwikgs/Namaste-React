b3171188e0b4c27ec5d3572ecef8ba922078d6dd
ritwikgs <ritwik.gs@gmail.com>
2024-01-14 14:04:26 +0530

    intro-to-useState-hook

--- src/components/Body.js
+++ src/components/Body.js
@@ -1,12 +1,38 @@
 import RestaurantCard from "./ResturantCard";
 import allRestaurants from "../utils/mock-data";
+import React, { useState } from "react";
 
 const Body = () => {
+  //state variable - use state hook
+  const [listOfRestaurants, setListOfRestaurants] = useState(
+    allRestaurants || []
+  );
   return (
     <div className="body">
       <div className="search">Search</div>
+      <div className="top-rated-restaurants-filter">
+        <button
+          className="top-rated-restaurants-btn"
+          onClick={() => {
+            const filteredList = listOfRestaurants?.filter(
+              (res) => Number(res?.ratings) >= 4.5
+            );
+            setListOfRestaurants(filteredList);
+          }}
+        >
+          Top rated Restaurants
+        </button>
+        <button
+          className="top-rated-restaurants-btn"
+          onClick={() => {
+            setListOfRestaurants(allRestaurants);
+          }}
+        >
+          All Restaurants
+        </button>
+      </div>
       <div className="restaurant-container">
-        {allRestaurants?.map((res, index) => (
+        {listOfRestaurants?.map((res, index) => (
           <RestaurantCard
             restaurantName={res?.restaurantName}
             cuisines={res?.cuisines}
